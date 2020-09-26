e6661d2a6f3ef9fd05545dd6bd7352607ef89a3e
sumeetjm <smungekar7@gmail.com>
2020-09-26 15:44:45 +0530

    Add altName

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -6,8 +6,26 @@ class AppConstants {
   static const DEFAULT_PROFILE_PIC =
       'https://mlrjx6kefml3.i.optimole.com/6AH3zQ-wlC6a5CH/w:300/h:300/q:auto/dpr:1.3/rt:fill/g:ce/https://stratefix.com/wp-content/uploads/2016/04/dummy-profile-pic-male1.jpg';
   static final List<Gender> genderList = [
-    Gender(id: null, name: 'Male', code: 'M', isActive: true),
-    Gender(id: null, name: 'Female', code: 'F', isActive: true),
-    Gender(id: null, name: 'Other', code: 'O', isActive: true),
+    Gender(
+      id: null,
+      name: 'Male',
+      code: 'M',
+      isActive: true,
+      altName: 'Men',
+    ),
+    Gender(
+      id: null,
+      name: 'Female',
+      code: 'F',
+      isActive: true,
+      altName: 'Women',
+    ),
+    Gender(
+      id: null,
+      name: 'Other',
+      code: 'O',
+      isActive: true,
+      altName: 'Other',
+    )
   ];
 }
